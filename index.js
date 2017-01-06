da83d1a4bc91225281e9f50ed5f0a0d6a07f3563
hetsketch <al3xander.koval@gmail.com>
2017-01-06 19:23:00 +0300

    'init'

---
+++ index.js
@@ -0,0 +1,33 @@
+'use strict';
+
+const ttf2svg = require('ttf2svg');
+const gutil = require('gulp-util');
+const through2 = require('through2').obj;
+
+const PLUGIN_NAME = 'gulp-ttf2svg';
+
+function convertToSvg(options) {
+  return through2(function (file, enc, cb) {
+    if (file.isNull()) {
+      cb(new gutil.PluginError('gulp-example-plugin', 'File is null'));
+      return;
+    }
+
+    if (file.isStream()) {
+      cb(new gutil.PluginError('gulp-example-plugin', 'Streaming not supported'));
+      return;
+    }
+    try {
+      //file.contents is an instance of a Buffer
+      let svg = ttf2svg(file.contents);
+      //ttf2svg returns String so wrap it into Buffer
+      file.contents = new Buffer(svg);
+      file.path = gutil.replaceExtension(file.path, '.svg');
+    } catch (err) {
+      this.emit('error', new gutil.PluginError('gulp-example-plugin', err));
+    }
+    cb(null, file);
+  })
+}
+
+module.exports = convertToSvg;
---
+++ test/test.js
@@ -0,0 +1,25 @@
+'use strict';
+
+const fs = require('fs');
+const assert = require('assert');
+const gutil = require('gulp-util');
+const ttf2svg = require('../');
+
+it('default test', done => {
+  const stream = ttf2svg();
+
+  stream.on('data', file => {
+    const fixtures = fs.readFileSync('./test/fixtures/Aller_Lt.svg');
+    console.log(file.contents.toString());
+    console.log(fixtures.toString());
+    assert.equal(file.contents.toString(), fixtures.toString());
+    done();
+  });
+
+  stream.write(new gutil.File({
+    path: './test/fixtures/Aller_Lt.ttf',
+    contents: fs.readFileSync('./test/fixtures/Aller_Lt.ttf')
+  }));
+
+  stream.end();
+});
